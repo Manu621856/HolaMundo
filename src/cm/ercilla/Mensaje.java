b6cececb4bd76c7452cbdafb53a14ed3d4d0991a
smr <adf@gmail.com>
2025-09-16 13:03:11 +0200

    Creo la clase mensaje

---
+++ src/cm/ercilla/Mensaje.java
@@ -0,0 +1,11 @@
+package cm.ercilla;
+
+import java.util.Scanner;
+
+public class Mensaje {
+    public String solicitardatos(String Mensaje){
+        System.out.print(mensaje);
+        Scanner scn = new Scanner(System.in);
+        return scn.nextLine()
+    }
+}
