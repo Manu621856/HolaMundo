b31f897a4b67ae267dee92dc0e78209e41454a3d
Manuel <moyamanuelalcaide@gmail.com>
2025-09-22 16:00:14 +0200

    se añadio una linea de codigo inservible

--- src/cm/ercilla/PersonaRegistro.java
+++ src/cm/ercilla/PersonaRegistro.java
@@ -2,6 +2,7 @@ package cm.ercilla;
 
 public record PersonaRegistro(String nombre, String apellidos) {
     public  String toString(){
+        System.out.println("Esta instruccion es inservible");
         return "%s %s" .formatted(nombre, apellidos);
     }
 
