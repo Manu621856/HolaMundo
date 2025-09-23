4e18d76108d8167270480bc5568748d8829d5b97
Manu621856 <moyamanuelalcaide@gmail.com>
2025-09-23 11:54:33 +0200

    Comentario

--- src/cm/ercilla/PersonaRegistro.java
+++ src/cm/ercilla/PersonaRegistro.java
@@ -2,7 +2,8 @@ package cm.ercilla;
 
 public record PersonaRegistro(String nombre, String apellidos) {
     public  String toString(){
-        System.out.println("Esta instruccion es inservible");
+        //juan comento la linea que se puede eliminar
+        //System.out.println("Esta instruccion es inservible");
         return "%s %s" .formatted(nombre, apellidos);
     }
 
